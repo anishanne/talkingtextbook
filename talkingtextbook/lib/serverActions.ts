594624e0492fc53a68f95cf08b0d2d9eb17dab95
Anish Anne <22670462+anishanne@users.noreply.github.com>
2024-06-30 19:41:55 -0400

    Moved from API to Server Actions

--- talkingtextbook/lib/serverActions.ts
+++ talkingtextbook/lib/serverActions.ts
@@ -1,8 +1,16 @@
 "use server";
 
+import z from "zod";
 import { connectToDatabase } from "./mongodb";
 
 export async function getTextbooks() {
 	const { textbooksDB } = await connectToDatabase();
 	return textbooksDB.find().toArray();
+}
+
+export async function createTextbooks() {
+	const { textbooksDB } = await connectToDatabase();
+	const { name } = z.object({ name: z.string() }).parse(await request.json());
+	const { insertedId } = await textbooksDB.insertOne({ name });
+	return insertedId;
 }
