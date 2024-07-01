90d3cba2fb5ccd33cd9ffdd0752b89a29ba72bcb
Anish Anne <22670462+anishanne@users.noreply.github.com>
2024-06-30 22:28:05 -0400

    dark theme

--- talkingtextbook/components/createTextbooks.jsx
+++ talkingtextbook/components/createTextbooks.jsx
@@ -16,19 +16,19 @@ export default function CreateTextbook({ open, setOpen }) {
 				<div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
 					<DialogPanel
 						transition
-						className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95">
+						className="relative transform overflow-hidden rounded-lg bg-black px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95">
 						<div>
 							<div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-purple-100">
 								<DocumentPlusIcon className="h-6 w-6 text-purple-600" aria-hidden="true" />
 							</div>
 							<div className="mt-3 text-center sm:mt-5">
-								<DialogTitle as="h3" className="text-base font-semibold leading-6 text-gray-900">
-									Create a textbook!
+								<DialogTitle as="h3" className="text-base font-semibold leading-6 text-gray-100">
+									Train a textbook!
 								</DialogTitle>
 								<div className="mt-2">
 									<p className="text-sm text-gray-500">
 										<div>
-											<label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+											<label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-200">
 												Textbook Name
 											</label>
 											<div className="mt-2">
@@ -36,18 +36,18 @@ export default function CreateTextbook({ open, setOpen }) {
 													type="text"
 													name="name"
 													id="name"
-													className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+													className="block w-full rounded-md border-0 bg-gray-800 py-1.5 text-gray-200 shadow-sm ring-1 ring-inset ring-gray-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 													placeholder="English 101"
 												/>
 											</div>
 										</div>
 										<div>
-											<label htmlFor="file" className="mt-4 block text-sm font-medium leading-6 text-gray-900">
+											<label htmlFor="file" className="mt-4 block text-sm font-medium leading-6 text-gray-100">
 												Textbook Upload
 											</label>
 											<div className="mt-2">
 												<input
-													class="block w-full cursor-pointer rounded-lg border border-gray-300 bg-gray-50 text-sm text-gray-900 focus:outline-none"
+													class="block w-full cursor-pointer rounded-lg border border-gray-700 bg-gray-800 text-sm text-gray-100 focus:outline-none"
 													id="file"
 													name="file"
 													type="file"
@@ -67,7 +67,7 @@ export default function CreateTextbook({ open, setOpen }) {
 							</button>
 							<button
 								type="button"
-								className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+								className="mt-3 inline-flex w-full justify-center rounded-md bg-black px-3 py-2 text-sm font-semibold text-gray-100 shadow-sm ring-1 ring-inset ring-gray-700 hover:bg-gray-900 sm:col-start-1 sm:mt-0"
 								onClick={() => setOpen(false)}
 								data-autofocus>
 								Cancel
--- talkingtextbook/components/viewTextbooks.tsx
+++ talkingtextbook/components/viewTextbooks.tsx
@@ -24,17 +24,17 @@ export default function ViewTextbooks({ open, setOpen }: { open: boolean; setOpe
 				<div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
 					<DialogPanel
 						transition
-						className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95">
+						className="relative transform overflow-hidden rounded-lg bg-gray-900 px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95">
 						<div>
 							<div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-indigo-100">
 								<DocumentTextIcon className="h-6 w-6 text-indigo-600" aria-hidden="true" />
 							</div>
 							<div className="mt-3 text-center sm:mt-5">
-								<DialogTitle as="h3" className="text-base font-semibold leading-6 text-gray-900">
+								<DialogTitle as="h3" className="text-base font-semibold leading-6 text-gray-100">
 									Created Textbooks
 								</DialogTitle>
 								<div className="mt-2">
-									<p className="text-sm text-gray-500">
+									<p className="text-sm text-gray-400">
 										{textbooks.length === 0 && "No Textbooks Found!"}
 										{textbooks.map((textbook) => (
 											<>
@@ -52,7 +52,7 @@ export default function ViewTextbooks({ open, setOpen }: { open: boolean; setOpe
 						<div className="mt-5">
 							<button
 								type="button"
-								className="inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+								className="inline-flex w-full justify-center rounded-md bg-black px-3 py-2 text-sm font-semibold text-gray-100 shadow-sm ring-1 ring-inset ring-gray-700 hover:bg-gray-800 sm:col-start-1 sm:mt-0"
 								onClick={() => setOpen(false)}
 								data-autofocus>
 								Close
